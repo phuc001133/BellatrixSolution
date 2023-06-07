e185606c491b302035b8426cc8b07c6970aa1399
Phuc Nguyen <phuc001133@gmail.com>
2023-06-07 11:35:09 +0700

    add WebdriverManager dependency and create testInit and testCleanup method

--- src/test/java/Chapter2.java
+++ src/test/java/Chapter2.java
@@ -1,4 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.util.concurrent.TimeUnit;
+
 public class Chapter2 {
-    
+    private WebDriver driver;
+    private static String purchaseEmail;
+    private static String purchaseOrderNumber;
+
+    @BeforeMethod
+    public void testInit() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void testCleanup() throws InterruptedException {
+        Thread.sleep(3000);
+    }
 
 }
